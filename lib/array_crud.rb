ee035adf8f1ea844972fec5c60853fda7fce16f8
Leann Kim <leannminakim@gmail.com>
2018-03-03 17:39:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,12 +23,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array ["one", "two"]
+  array = ["one", "two"]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["one", "two", "three"]
+  array [0]
 end
 
 def retrieve_first_element_from_array(array)
